d884138a6006e18bc7f088956dab379c467ab169
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-01-23 22:32:30 +0100

    Update script.js
    Created function that updates letterContainer when correct letter is clicked.
    Created function checkLetter, however not complete

--- script.js
+++ script.js
@@ -68,28 +68,40 @@ let guessedWords = [];
 let guessedLetters = [];
 let usedWords = [];
 let activeWord
-
+let wordArray = [];
 let count = 0;
 let points = 0;
+let letterContainer;
+let outputArray = []; // Array that is viewed in UI
 
 function generateWord() {
     let randomIndex = (Math.floor(Math.random() * availableWords.length)); //Gets random number between 0 and 4 (?) corresponding to an index.
-    let wordContainer = document.querySelector(".word"); //Creates a container for the activeWord
-    activeWord = availableWords[randomIndex]; //Set active word to random word from list availableWords through random index.
-    wordContainer.innerHTML = activeWord; //Insert activeWord to wordContainer
-       
+    activeWord = availableWords[randomIndex];
+    wordArray = activeWord.split(""); //Changes activeWord to array.
+    console.log(wordArray);
+    for (let i = 0; i<wordArray.length; i++) {
+      outputArray.push(" ");
+    };
+
+    updateLetterContainer(); //Creates boxes for each letter
+
     usedWords.push(activeWord); //Adds activeWord to usedWords
     availableWords.splice(randomIndex, 1); //Removes activeWord from 
 };
 
-// function checkLetter() {
-//     document.addEventListener("keydown", function(letter) {
-//         console.log("Key pressed: " + letter.key);
-//     });
-// }
+function updateLetterContainer(){
+  let wordContainer = document.querySelector(".word");
+  wordContainer.replaceChildren(); //Removes all the boxes
+  for(let i = 0; i < outputArray.length; i++) {
+    letterContainer = document.createElement("li"); //Creates a listelement
+    wordContainer.append(letterContainer); //Adds listelement to the wordContainer
+    letterContainer.innerHTML = outputArray[i]; //Adds letter to listelement in wordContainer
+  };
+};
 
 generateWord();
 
+//Creates keyboard and adds an eventlistener to each key
 alphabet.forEach((letter) => {
     let key = document.createElement("div");
     key.classList.add("key");
@@ -97,6 +109,18 @@ alphabet.forEach((letter) => {
     document.querySelector(".keyboard").append(key);
 
     key.addEventListener("click", ()=> {
-      //create function checkLetter
+      let clickedLetter = key.innerHTML; // Kanske bryta ut funktionen till checkLetter??
+      for (let i = 0; i < wordArray.length; i++) {
+        if(wordArray[i] === clickedLetter) {
+          outputArray[i] = clickedLetter;
+          updateLetterContainer();
+        };
+      };
     });
 });
+
+// function checkLetter() {
+  
+// }
+
+
