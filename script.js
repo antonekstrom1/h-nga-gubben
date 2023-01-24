eb43086078f48930b7ac007164c29c89f73acc45
antonekstrom1 <antonekstrom98@gmail.com>
2023-01-24 14:01:30 +0100

    update
    small update

--- script.js
+++ script.js
@@ -9,35 +9,35 @@
 // Skapa en funktion som tar count som argument 
 
 const alphabet = [ //Borde vara const istället?
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  let availableWords = ["BANANA", "DOG", "POKEMON", "CLOUDS"]; //Ska den vara const?
-  let availableLetters = [ //Borde vara const istället?
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let availableWords = ["BANANA", "DOG", "POKEMON", "CLOUDS"]; //Ska den vara const?
+let availableLetters = [ //Borde vara const istället?
   "A",
   "B",
   "C",
@@ -78,32 +78,29 @@ let guessedLetterContainer;
 let outputArray = []; // Array that is viewed in UI
 let clickedLetter;
 
-let pointContainer = document.createElement("h3");
-document.querySelector("main").append(pointContainer);
-pointContainer.innerHTML = points;
+document.querySelector(".score").innerHTML = ("Your score: ") + points;
 
 generateWord();
 
-
 function generateWord() {
-    let randomIndex = (Math.floor(Math.random() * availableWords.length)); //Gets random number between 0 and 4 (?) corresponding to an index.
-    activeWord = availableWords[randomIndex];
-    wordArray = activeWord.split(""); //Changes activeWord to array.
-    console.log(wordArray);
-    for (let i = 0; i<wordArray.length; i++) {
-      outputArray.push(" ");
-    };
+  let randomIndex = (Math.floor(Math.random() * availableWords.length)); //Gets random number between 0 and 4 (?) corresponding to an index.
+  activeWord = availableWords[randomIndex];
+  wordArray = activeWord.split(""); //Changes activeWord to array.
+  console.log(wordArray);
+  for (let i = 0; i < wordArray.length; i++) {
+    outputArray.push(" ");
+  };
 
-    updateLetterContainer(); //Creates boxes for each letter
+  updateLetterContainer(); //Creates boxes for each letter
 
-    usedWords.push(activeWord); //Adds activeWord to usedWords
-    availableWords.splice(randomIndex, 1); //Removes activeWord from availableWords
+  usedWords.push(activeWord); //Adds activeWord to usedWords
+  availableWords.splice(randomIndex, 1); //Removes activeWord from availableWords
 };
 
-function updateLetterContainer(){
+function updateLetterContainer() {
   let wordContainer = document.querySelector(".word");
   wordContainer.replaceChildren(); //Removes all the boxes
-  for(let i = 0; i < outputArray.length; i++) {
+  for (let i = 0; i < outputArray.length; i++) {
     letterContainer = document.createElement("li"); //Creates a listelement
     wordContainer.append(letterContainer); //Adds listelement to the wordContainer
     letterContainer.innerHTML = outputArray[i]; //Adds letter to listelement in wordContainer
@@ -119,42 +116,42 @@ function updateGuessedContainer() {
 
 //Creates keyboard and adds an eventlistener to each key - kan vi bryta ut funktionerna?
 alphabet.forEach((letter) => {
-    let key = document.createElement("div");
-    key.classList.add("key");
-    key.innerHTML = letter;
-    document.querySelector(".keyboard").append(key);
+  let key = document.createElement("div");
+  key.classList.add("key");
+  key.innerHTML = letter;
+  document.querySelector(".keyboard").append(key);
 
-    key.addEventListener("click", ()=> {
-      clickedLetter = key.innerHTML; // Kanske bryta ut funktionen till checkLetter??
-      key.setAttribute("class","active"); //Adds class active to clicked letter
-      
-      for (let i = 0; i < wordArray.length; i++) {
-        if(wordArray[i] === clickedLetter) {
-          outputArray[i] = clickedLetter;
-          updateLetterContainer();
-        }  
-      };
-      if(wordArray.includes(clickedLetter) == false) {
-        updateGuessedContainer();
-        count = count + 1;
-        hangman(count); //Draw hangman component
-      } 
-      
-      if(compareArrays(wordArray, outputArray)) {
-        points = points + 1;
-        pointContainer.innerHTML = points;
-        gameWin();
+  key.addEventListener("click", () => {
+    clickedLetter = key.innerHTML; // Kanske bryta ut funktionen till checkLetter??
+    key.setAttribute("class", "active"); //Adds class active to clicked letter
+
+    for (let i = 0; i < wordArray.length; i++) {
+      if (wordArray[i] === clickedLetter) {
+        outputArray[i] = clickedLetter;
+        updateLetterContainer();
       }
-    });
+    };
+    if (wordArray.includes(clickedLetter) == false) {
+      updateGuessedContainer();
+      count = count + 1;
+      hangman(count); //Draw hangman component
+    }
+
+    if (compareArrays(wordArray, outputArray)) {
+      points = points + 1;
+      pointContainer.innerHTML = points;
+      gameWin();
+    }
   });
+});
 
 
-function compareArrays(arr1,arr2) {
+function compareArrays(arr1, arr2) {
   return arr1.toString() === arr2.toString();
 };
 
 function hangman(count) {
-  if(count === 1){
+  if (count === 1) {
     document.querySelector('figure').classList.add('scaffold')
   } else if (count === 2) {
     document.querySelector('figure').classList.add('head')
